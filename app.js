138ec5211b346c10a1186ea3acc2956c554b47b3
afmera <felipemera@unimayor.edu.co>
2019-08-30 15:01:02 -0500

    Segundo pequeño avance.

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-document.write("<h1>Hello World</h1>");
+//function
+var suma = function (num1, num2) {
+    var numero1 = parseInt( document.getElementById("numero_1").value);
+    var numero2 = parseInt( document.getElementById("numero_2").value);
+    var resultado=numero1+numero2;
+    return resultado;
+}
